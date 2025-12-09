20eef7b2c41286bf665348b0dc6dbee38d3591a5
ashish khatarker <ashishkhatarker90@gmail.com>
2025-12-09 08:19:18 +0530

    add PatientController

---
+++ patient-service/src/main/java/com/ash/patient_service/controller/PatientController.java
@@ -0,0 +1,17 @@
+package com.ash.patient_service.controller;
+
+import com.ash.patient_service.service.PatientService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+
+}
