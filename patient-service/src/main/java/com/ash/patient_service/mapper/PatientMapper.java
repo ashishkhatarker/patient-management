da9cc2053d5dbda34e10d07f30b6687132b37fd5
ashish khatarker <ashishkhatarker90@gmail.com>
2025-12-09 09:21:19 +0530

    add PatientMapper for patientRequestDTO to patient model

--- patient-service/src/main/java/com/ash/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/ash/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.ash.patient_service.mapper;
 
+import com.ash.patient_service.dto.PatientRequestDTO;
 import com.ash.patient_service.dto.PatientResponseDTO;
 import com.ash.patient_service.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -14,5 +17,17 @@ public class PatientMapper {
         patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO requestDTO){
+        Patient patient = new Patient();
+        patient.setName(requestDTO.getName());
+        patient.setEmail(requestDTO.getEmail());
+        patient.setAddress(requestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(requestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(requestDTO.getRegisteredDate()));
+
+
+        return patient;
     }
 }
