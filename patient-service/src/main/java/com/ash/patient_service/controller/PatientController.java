76e575925e14af22b1e6175d9cebfbec324dd299
ashish khatarker <ashishkhatarker90@gmail.com>
2025-12-09 08:29:33 +0530

    add get all patients end-point

--- patient-service/src/main/java/com/ash/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/ash/patient_service/controller/PatientController.java
@@ -1,11 +1,16 @@
 package com.ash.patient_service.controller;
 
+import com.ash.patient_service.dto.PatientResponseDTO;
 import com.ash.patient_service.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/patient")
+@RequestMapping("/patients")
 public class PatientController {
     private final PatientService patientService;
 
@@ -13,5 +18,11 @@ public class PatientController {
         this.patientService = patientService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+       List<PatientResponseDTO> patients = patientService.getPatients();
+
+       return ResponseEntity.ok().body(patients);
+    }
 
 }
--- patient-service/src/main/java/com/ash/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/ash/patient_service/service/PatientService.java
@@ -1,7 +1,13 @@
 package com.ash.patient_service.service;
 
+import com.ash.patient_service.dto.PatientResponseDTO;
+import com.ash.patient_service.mapper.PatientMapper;
+import com.ash.patient_service.model.Patient;
 import com.ash.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -10,5 +16,16 @@ public class PatientService {
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+
+//        List<PatientResponseDTO> responseDTOs = patients.stream()
+//                .map(PatientMapper::toDTO).toList();
+//        return  responseDTOs;
+
+        return patients.stream()
+                .map(PatientMapper::toDTO).toList();
     }
 }
