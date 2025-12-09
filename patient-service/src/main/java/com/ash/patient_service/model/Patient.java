7e609126861fa8d47de7a1e0c41dc3fff77a4230
ashish khatarker <ashishkhatarker90@gmail.com>
2025-12-09 07:47:03 +0530

    add h2 database and configuration properties

--- patient-service/src/main/java/com/ash/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/ash/patient_service/model/Patient.java
@@ -31,8 +31,51 @@ public class Patient {
     @NotNull
     private LocalDate registeredDate;
 
+    public UUID getId() {
+        return id;
+    }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
+    public @NotNull String getName() {
+        return name;
+    }
 
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
 
+    public @NotNull @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull @Email String email) {
+        this.email = email;
+    }
+
+    public @NotNull String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotNull String address) {
+        this.address = address;
+    }
+
+    public @NotNull LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(@NotNull LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public @NotNull LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(@NotNull LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
 }
