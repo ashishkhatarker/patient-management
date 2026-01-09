bbe1985e9ad9f11f84305f9483abf90e8869ac79
ashish khatarker <ashishkhatarker90@gmail.com>
2026-01-09 19:54:23 +0530

    add auth-service base file

---
+++ auth-service/src/test/java/com/ash/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
